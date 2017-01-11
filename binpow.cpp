27c73dfb5b3293514af349c7605c0c143c18d07d
Mikhailichenko <anast1221@gmail.com>
2017-01-11 20:41:39 +0400

    Update binpow.cpp

--- binpow.cpp
+++ binpow.cpp
@@ -6,7 +6,7 @@ double binpow (double a, int n){
    if (n<0) return 1/binpow(a, -n);
    if (n==0) return 1;
    if (n%2==0) return binpow(a, n/2)*binpow(a, n/2);
-   return a*binpow(a, (n-1)/2);
+   return a*binpow(a, n/2)*bipow(a, n/2);
 
 }
 
